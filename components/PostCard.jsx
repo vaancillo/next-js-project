843de22d28ecd9ae433db48ecab0eb7dadf17034
vancillo <yaredbane@gmail.com>
2023-08-24 23:41:05 -0600

    params

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,13 @@
 "use client"
+import Link from "next/link"
 
 // RCC
 function PostCard({ post }) {
   return (
     <div>
+    <Link href={`/post/${post.id}`}>
     <h3>{post.id}. {post.title}</h3>
+    </Link>
     <p>{post.body}</p>
     <button onClick={() => {
         alert('funcionando!')
